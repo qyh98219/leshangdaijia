5fcda76a757df07c293ce00adc808984d34fe8f5
qyh98219 <1649026770@qq.com>
2024-08-13 15:21:53 +0800

    登录功能远程调用完成

--- web/web-customer/src/main/java/com/atguigu/daijia/customer/service/CustomerService.java
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/service/CustomerService.java
@@ -2,5 +2,5 @@ package com.atguigu.daijia.customer.service;
 
 public interface CustomerService {
 
-
+    String login(String code);
 }
--- web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CustomerServiceImpl.java
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CustomerServiceImpl.java
@@ -1,16 +1,46 @@
 package com.atguigu.daijia.customer.service.impl;
 
+import com.atguigu.daijia.common.constant.RedisConstant;
+import com.atguigu.daijia.common.execption.GuiguException;
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.common.result.ResultCodeEnum;
 import com.atguigu.daijia.customer.client.CustomerInfoFeignClient;
 import com.atguigu.daijia.customer.service.CustomerService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class CustomerServiceImpl implements CustomerService {
 
+    @Autowired
+    private CustomerInfoFeignClient customerInfoFeignClient;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
+    @Override
+    public String login(String code) {
+        Result<Long> loginResult = customerInfoFeignClient.login(code);
+        Integer statusCode = loginResult.getCode();
+        if (!statusCode.equals(HttpStatus.SC_OK)){
+            throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+        }
 
+        Long customerId = loginResult.getData();
+        if (Objects.isNull(customerId)){
+            throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+        }
+
+        String token = UUID.randomUUID().toString().replaceAll("-", "");
+        redisTemplate.opsForValue().set(RedisConstant.USER_LOGIN_KEY_PREFIX+token, customerId.toString(), RedisConstant.USER_LOGIN_KEY_TIMEOUT, TimeUnit.SECONDS);
+        return token;
+    }
 }
