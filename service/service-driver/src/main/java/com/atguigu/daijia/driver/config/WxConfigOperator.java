7957d66eb6c57a153c547e7d4d01138856b46e44
qyh98219 <1649026770@qq.com>
2024-08-16 15:29:20 +0800

    司机登录服务接口完成

---
+++ service/service-driver/src/main/java/com/atguigu/daijia/driver/config/WxConfigOperator.java
@@ -0,0 +1,29 @@
+package com.atguigu.daijia.driver.config;
+
+import cn.binarywang.wx.miniapp.api.WxMaService;
+import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
+import cn.binarywang.wx.miniapp.config.impl.WxMaDefaultConfigImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author qyh
+ * @version 1.0
+ * @className WxConfigOperator
+ * @description TODO
+ * @date 2024/8/12 15:27
+ **/
+@Component
+public class WxConfigOperator {
+
+    @Bean
+    public WxMaService wxMaService(WxConfigProperties properties) {
+        WxMaDefaultConfigImpl wxMaConfig = new WxMaDefaultConfigImpl();
+        wxMaConfig.setAppid(properties.getAppId());
+        wxMaConfig.setSecret(properties.getSecret());
+
+        WxMaService wxMaService = new WxMaServiceImpl();
+        wxMaService.setWxMaConfig(wxMaConfig);
+        return wxMaService;
+    }
+}
--- service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/DriverInfoServiceImpl.java
+++ service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/DriverInfoServiceImpl.java
@@ -1,17 +1,83 @@
 package com.atguigu.daijia.driver.service.impl;
 
+import cn.binarywang.wx.miniapp.api.WxMaService;
+import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
+import com.atguigu.daijia.common.constant.SystemConstant;
+import com.atguigu.daijia.driver.mapper.DriverAccountMapper;
 import com.atguigu.daijia.driver.mapper.DriverInfoMapper;
+import com.atguigu.daijia.driver.mapper.DriverLoginLogMapper;
+import com.atguigu.daijia.driver.mapper.DriverSetMapper;
 import com.atguigu.daijia.driver.service.DriverInfoService;
+import com.atguigu.daijia.model.entity.driver.DriverAccount;
 import com.atguigu.daijia.model.entity.driver.DriverInfo;
+import com.atguigu.daijia.model.entity.driver.DriverLoginLog;
+import com.atguigu.daijia.model.entity.driver.DriverSet;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.error.WxErrorException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Objects;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class DriverInfoServiceImpl extends ServiceImpl<DriverInfoMapper, DriverInfo> implements DriverInfoService {
+    @Autowired
+    private WxMaService wxMaService;
+    @Autowired
+    private DriverSetMapper driverSetMapper;
+    @Autowired
+    private DriverAccountMapper driverAccountMapper;
+    @Autowired
+    private DriverLoginLogMapper driverLoginLogMapper;
 
+    @Override
+    public Long login(String code) {
+        String openId = "";
+        try {
+            WxMaJscode2SessionResult sessionInfo = wxMaService.getUserService().getSessionInfo(code);
+            openId = sessionInfo.getOpenid();
+        } catch (WxErrorException e) {
+            throw new RuntimeException(e);
+        }
+        LambdaQueryWrapper<DriverInfo> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(DriverInfo::getWxOpenId,openId);
+        DriverInfo driverInfo = this.getOne(queryWrapper);
 
+        if(Objects.isNull(driverInfo)){
+            //添加司机基本信息
+            driverInfo = new DriverInfo();
+            driverInfo.setNickname(String.valueOf(System.currentTimeMillis()));
+            driverInfo.setAvatarUrl("https://oss.aliyuncs.com/aliyun_id_photo_bucket/default_handsome.jpg");
+            driverInfo.setWxOpenId(openId);
+            this.save(driverInfo);
 
+            //初始化默认设置
+            DriverSet driverSet = new DriverSet();
+            driverSet.setDriverId(driverInfo.getId());
+            //0：无限制
+            driverSet.setOrderDistance(new BigDecimal(0));
+            //默认接单范围：5公里
+            driverSet.setAcceptDistance(new BigDecimal(SystemConstant.ACCEPT_DISTANCE));
+            //0：否 1：是
+            driverSet.setIsAutoAccept(0);
+            driverSetMapper.insert(driverSet);
+
+            //初始化司机账户
+            DriverAccount driverAccount = new DriverAccount();
+            driverAccount.setDriverId(driverInfo.getId());
+            driverAccountMapper.insert(driverAccount);
+        }
+
+        //登录日志
+        DriverLoginLog driverLoginLog = new DriverLoginLog();
+        driverLoginLog.setDriverId(driverInfo.getId());
+        driverLoginLog.setMsg("小程序登录");
+        driverLoginLogMapper.insert(driverLoginLog);
+        return driverInfo.getId();
+    }
 }
