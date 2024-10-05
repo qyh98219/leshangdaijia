1638c0eeac92c410ca8bbddfde8e2d18a6d38883
qyh <1649026770@qq.com>
2024-10-05 13:33:04 +0800

    修改更新司机位置接口，添加判断司机是否营业

--- web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/LocationServiceImpl.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/LocationServiceImpl.java
@@ -1,9 +1,13 @@
 package com.atguigu.daijia.driver.service.impl;
 
+import com.atguigu.daijia.common.constant.RedisConstant;
+import com.atguigu.daijia.common.execption.GuiguException;
+import com.atguigu.daijia.common.result.ResultCodeEnum;
 import com.atguigu.daijia.driver.client.DriverInfoFeignClient;
 import com.atguigu.daijia.driver.service.LocationService;
 import com.atguigu.daijia.map.client.LocationFeignClient;
 import com.atguigu.daijia.map.client.MapFeignClient;
+import com.atguigu.daijia.model.entity.driver.DriverSet;
 import com.atguigu.daijia.model.form.map.UpdateDriverLocationForm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +19,16 @@ import org.springframework.stereotype.Service;
 public class LocationServiceImpl implements LocationService {
     @Autowired
     private LocationFeignClient locationFeignClient;
+    @Autowired
+    private DriverInfoFeignClient driverInfoFeignClient;
 
     @Override
     public Boolean updateDriverLocation(UpdateDriverLocationForm updateDriverLocationForm) {
-        return locationFeignClient.updateDriverLocation(updateDriverLocationForm).getData();
+        DriverSet driverSet = driverInfoFeignClient.getDriverSet(updateDriverLocationForm.getDriverId()).getData();
+        if (driverSet.getServiceStatus() == 1) {
+            return locationFeignClient.updateDriverLocation(updateDriverLocationForm).getData();
+        } else {
+            throw new GuiguException(ResultCodeEnum.NO_START_SERVICE);
+        }
     }
 }
