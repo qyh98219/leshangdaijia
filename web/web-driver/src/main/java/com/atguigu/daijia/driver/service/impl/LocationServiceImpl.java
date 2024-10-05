f7b188336e02c4f0be677de698c1464421b6c79d
qyh <1649026770@qq.com>
2024-10-05 13:18:22 +0800

    获取司机个性化设置接口完成

--- web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/LocationServiceImpl.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/LocationServiceImpl.java
@@ -1,7 +1,10 @@
 package com.atguigu.daijia.driver.service.impl;
 
+import com.atguigu.daijia.driver.client.DriverInfoFeignClient;
 import com.atguigu.daijia.driver.service.LocationService;
 import com.atguigu.daijia.map.client.LocationFeignClient;
+import com.atguigu.daijia.map.client.MapFeignClient;
+import com.atguigu.daijia.model.form.map.UpdateDriverLocationForm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +13,11 @@ import org.springframework.stereotype.Service;
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class LocationServiceImpl implements LocationService {
+    @Autowired
+    private LocationFeignClient locationFeignClient;
 
-
+    @Override
+    public Boolean updateDriverLocation(UpdateDriverLocationForm updateDriverLocationForm) {
+        return locationFeignClient.updateDriverLocation(updateDriverLocationForm).getData();
+    }
 }
