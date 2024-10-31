5727ab1a9a1a9cb5da24bd22d7bc9920f89e3d10
qyh98219 <1649026770@qq.com>
2024-10-31 10:31:29 +0800

    司乘同显-乘客端计算最佳路径接口

--- web/web-customer/src/main/java/com/atguigu/daijia/customer/service/OrderService.java
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/service/OrderService.java
@@ -2,7 +2,9 @@ package com.atguigu.daijia.customer.service;
 
 import com.atguigu.daijia.model.form.customer.ExpectOrderForm;
 import com.atguigu.daijia.model.form.customer.SubmitOrderForm;
+import com.atguigu.daijia.model.form.map.CalculateDrivingLineForm;
 import com.atguigu.daijia.model.vo.customer.ExpectOrderVo;
+import com.atguigu.daijia.model.vo.map.DrivingLineVo;
 import com.atguigu.daijia.model.vo.map.OrderLocationVo;
 import com.atguigu.daijia.model.vo.order.OrderInfoVo;
 
@@ -17,4 +19,6 @@ public interface OrderService {
     OrderInfoVo getOrderInfo(Long orderId, Long customerId);
 
     OrderLocationVo getCacheOrderLocation(Long orderId);
+
+    DrivingLineVo calculateDrivingLine(CalculateDrivingLineForm calculateDrivingLineForm);
 }
