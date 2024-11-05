96d12cfe648cca24a0f5192533a8e8630a6b978e
qyh98219 <1649026770@qq.com>
2024-11-05 14:26:35 +0800

    订单审核统计功能

--- service/service-order/src/main/java/com/atguigu/daijia/order/service/OrderMonitorService.java
+++ service/service-order/src/main/java/com/atguigu/daijia/order/service/OrderMonitorService.java
@@ -2,11 +2,15 @@ package com.atguigu.daijia.order.service;
 
 import com.atguigu.daijia.model.entity.order.OrderMonitor;
 import com.atguigu.daijia.model.entity.order.OrderMonitorRecord;
-import com.atguigu.daijia.model.form.order.OrderMonitorForm;
 import com.baomidou.mybatisplus.extension.service.IService;
-import org.springframework.web.multipart.MultipartFile;
 
 public interface OrderMonitorService extends IService<OrderMonitor> {
 
     Boolean saveOrderMonitorRecord(OrderMonitorRecord orderMonitorRecord);
+
+    OrderMonitor getOrderMonitor(Long orderId);
+
+    Boolean updateOrderMonitor(OrderMonitor orderMonitor);
+
+    Long saveOrderMonitor(OrderMonitor orderMonitor);
 }
--- web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/MonitorServiceImpl.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/MonitorServiceImpl.java
@@ -3,6 +3,7 @@ package com.atguigu.daijia.driver.service.impl;
 import com.atguigu.daijia.driver.client.CiFeignClient;
 import com.atguigu.daijia.driver.service.FileService;
 import com.atguigu.daijia.driver.service.MonitorService;
+import com.atguigu.daijia.model.entity.order.OrderMonitor;
 import com.atguigu.daijia.model.entity.order.OrderMonitorRecord;
 import com.atguigu.daijia.model.form.order.OrderMonitorForm;
 import com.atguigu.daijia.model.vo.order.TextAuditingVo;
@@ -29,6 +30,7 @@ public class MonitorServiceImpl implements MonitorService {
         //上传文件
         String url = fileService.upload(file);
 
+
         OrderMonitorRecord orderMonitorRecord = new OrderMonitorRecord();
         orderMonitorRecord.setOrderId(orderMonitorForm.getOrderId());
         orderMonitorRecord.setFileUrl(url);
@@ -38,6 +40,17 @@ public class MonitorServiceImpl implements MonitorService {
         orderMonitorRecord.setResult(textAuditingVo.getResult());
         orderMonitorRecord.setKeywords(textAuditingVo.getKeywords());
         orderMonitorFeignClient.saveMonitorRecord(orderMonitorRecord);
+
+        //更新订单监控统计
+        OrderMonitor orderMonitor = orderMonitorFeignClient.getOrderMonitor(orderMonitorForm.getOrderId()).getData();
+        int fileNum = orderMonitor.getFileNum() + 1;
+        orderMonitor.setFileNum(fileNum);
+        //审核结果: 0（审核正常），1 （判定为违规敏感文件），2（疑似敏感，建议人工复核）。
+        if("3".equals(orderMonitorRecord.getResult())) {
+            int auditNum = orderMonitor.getAuditNum() + 1;
+            orderMonitor.setAuditNum(auditNum);
+        }
+        orderMonitorFeignClient.updateOrderMonitor(orderMonitor);
         return true;
     }
 }
