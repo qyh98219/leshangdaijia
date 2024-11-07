484a0eb9f867cc9a2226ac36fb8d0fca9541b621
qyh98219 <1649026770@qq.com>
2024-11-07 10:52:50 +0800

    计算订单实际距离接口完成

--- service/service-map/src/main/java/com/atguigu/daijia/map/repository/OrderServiceLocationRepository.java
+++ service/service-map/src/main/java/com/atguigu/daijia/map/repository/OrderServiceLocationRepository.java
@@ -4,7 +4,14 @@ import com.atguigu.daijia.model.entity.map.OrderServiceLocation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderServiceLocationRepository extends MongoRepository<OrderServiceLocation, String> {
-
+    /**
+     * 根据订单id查找保存的经纬度信息
+     * @param orderId
+     * @return
+     */
+    List<OrderServiceLocation> findOrderIdOrderByCreateTimeAsc(Long orderId);
 }
