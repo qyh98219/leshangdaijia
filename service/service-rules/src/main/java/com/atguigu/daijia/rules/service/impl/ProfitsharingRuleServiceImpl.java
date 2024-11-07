1136d80bc91324f1bdbc4cd31a2dcd3a5b01e982
qyh98219 <1649026770@qq.com>
2024-11-07 15:37:24 +0800

    分账接口完成

--- service/service-rules/src/main/java/com/atguigu/daijia/rules/service/impl/ProfitsharingRuleServiceImpl.java
+++ service/service-rules/src/main/java/com/atguigu/daijia/rules/service/impl/ProfitsharingRuleServiceImpl.java
@@ -1,8 +1,17 @@
 package com.atguigu.daijia.rules.service.impl;
 
+import com.atguigu.daijia.model.entity.rule.ProfitsharingRule;
+import com.atguigu.daijia.model.form.rules.ProfitsharingRuleRequest;
+import com.atguigu.daijia.model.form.rules.ProfitsharingRuleRequestForm;
+import com.atguigu.daijia.model.vo.rules.ProfitsharingRuleResponse;
+import com.atguigu.daijia.model.vo.rules.ProfitsharingRuleResponseVo;
 import com.atguigu.daijia.rules.mapper.ProfitsharingRuleMapper;
 import com.atguigu.daijia.rules.service.ProfitsharingRuleService;
+import com.atguigu.daijia.rules.utils.DroolsHelper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +19,35 @@ import org.springframework.stereotype.Service;
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class ProfitsharingRuleServiceImpl implements ProfitsharingRuleService {
+    private static final String RULES_CUSTOMER_RULES_DRL = "rules/ProfitsharingRule.drl";
 
     @Autowired
     private ProfitsharingRuleMapper rewardRuleMapper;
 
 
+
+    @Override
+    public ProfitsharingRuleResponseVo calculateOrderProfitsharingFee(ProfitsharingRuleRequestForm profitsharingRuleRequestForm) {
+        //封装传入对象
+        ProfitsharingRuleRequest profitsharingRuleRequest = new ProfitsharingRuleRequest();
+        profitsharingRuleRequest.setOrderAmount(profitsharingRuleRequestForm.getOrderAmount());
+        profitsharingRuleRequest.setOrderNum(profitsharingRuleRequestForm.getOrderNum());
+
+        KieSession kieSession = DroolsHelper.loadForRule(RULES_CUSTOMER_RULES_DRL);
+
+        //封装返回对象
+        ProfitsharingRuleResponse profitsharingRuleResponse = new ProfitsharingRuleResponse();
+        kieSession.setGlobal("profitsharingRuleResponse", profitsharingRuleResponse);
+        // 设置订单对象
+        kieSession.insert(profitsharingRuleRequest);
+        // 触发规则
+        kieSession.fireAllRules();
+        // 中止会话
+        kieSession.dispose();
+
+        //封装返回对象
+        ProfitsharingRuleResponseVo profitsharingRuleResponseVo = new ProfitsharingRuleResponseVo();
+        BeanUtils.copyProperties(profitsharingRuleResponse, profitsharingRuleResponseVo);
+        return profitsharingRuleResponseVo;
+    }
 }
