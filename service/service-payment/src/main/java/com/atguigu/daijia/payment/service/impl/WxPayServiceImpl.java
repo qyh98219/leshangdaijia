3683a8c6da777916b815706100330854a3172e55
qyh98219 <1649026770@qq.com>
2024-11-08 16:43:51 +0800

    微信支付结果通知接口完成

--- service/service-payment/src/main/java/com/atguigu/daijia/payment/service/impl/WxPayServiceImpl.java
+++ service/service-payment/src/main/java/com/atguigu/daijia/payment/service/impl/WxPayServiceImpl.java
@@ -1,7 +1,10 @@
 package com.atguigu.daijia.payment.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.atguigu.daijia.common.constant.MqConst;
 import com.atguigu.daijia.common.execption.GuiguException;
 import com.atguigu.daijia.common.result.ResultCodeEnum;
+import com.atguigu.daijia.common.service.RabbitService;
 import com.atguigu.daijia.model.entity.payment.PaymentInfo;
 import com.atguigu.daijia.model.form.payment.PaymentInfoForm;
 import com.atguigu.daijia.model.vo.payment.WxPrepayVo;
@@ -10,14 +13,22 @@ import com.atguigu.daijia.payment.mapper.PaymentInfoMapper;
 import com.atguigu.daijia.payment.service.WxPayService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.wechat.pay.java.core.RSAAutoCertificateConfig;
+import com.wechat.pay.java.core.notification.NotificationParser;
+import com.wechat.pay.java.core.notification.RequestParam;
 import com.wechat.pay.java.service.partnerpayments.jsapi.JsapiServiceExtension;
 import com.wechat.pay.java.service.partnerpayments.jsapi.model.*;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.RequestUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Service
 @Slf4j
@@ -28,6 +39,8 @@ public class WxPayServiceImpl implements WxPayService {
     private RSAAutoCertificateConfig rsaAutoCertificateConfig;
     @Autowired
     private WxPayV3Properties wxPayV3Properties;
+    @Autowired
+    private RabbitService rabbitService;
 
     @Override
     public WxPrepayVo createWxPayment(PaymentInfoForm paymentInfoForm) {
@@ -82,5 +95,75 @@ public class WxPayServiceImpl implements WxPayService {
             throw new GuiguException(ResultCodeEnum.DATA_ERROR);
         }
     }
+
+    @Override
+    public void wxnotify(HttpServletRequest request) {
+        //从request头信息获取参数
+        //HTTP 头 Wechatpay-Signature
+        // HTTP 头 Wechatpay-Nonce
+        //HTTP 头 Wechatpay-Timestamp
+        //HTTP 头 Wechatpay-Serial
+        //HTTP 头 Wechatpay-Signature-Type
+        //HTTP 请求体 body。切记使用原始报文，不要用 JSON 对象序列化后的字符串，避免验签的 body 和原文不一致。
+        String wechatPaySerial = request.getHeader("Wechatpay-Serial");
+        String nonce = request.getHeader("Wechatpay-Nonce");
+        String timestamp = request.getHeader("Wechatpay-Timestamp");
+        String signature = request.getHeader("Wechatpay-Signature");
+        String requestBody = this.readData(request);
+
+
+        //2.构造 RequestParam
+        RequestParam requestParam = new RequestParam.Builder()
+                .serialNumber(wechatPaySerial)
+                .nonce(nonce)
+                .signature(signature)
+                .timestamp(timestamp)
+                .body(requestBody)
+                .build();
+
+
+        //3.初始化 NotificationParser
+        NotificationParser parser = new NotificationParser(rsaAutoCertificateConfig);
+        //4.以支付通知回调为例，验签、解密并转换成 Transaction
+        Transaction transaction = parser.parse(requestParam, Transaction.class);
+
+        if(null != transaction && transaction.getTradeState() == Transaction.TradeStateEnum.SUCCESS) {
+            //5.处理支付业务
+            this.handlePayment(transaction);
+        }
+    }
+
+    private String readData(HttpServletRequest request) {
+        StringBuilder data = new StringBuilder();
+        BufferedReader reader = null;
+        try {
+            reader = request.getReader();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                data.append(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return data.toString();
+    }
+
+    public void handlePayment(Transaction transaction) {
+        PaymentInfo paymentInfo = paymentInfoMapper.selectOne(new LambdaQueryWrapper<PaymentInfo>().eq(PaymentInfo::getOrderNo, transaction.getOutTradeNo()));
+        if (paymentInfo.getPaymentStatus() == 1) {
+            return;
+        }
+
+        //更新支付信息
+        paymentInfo.setPaymentStatus(1);
+        paymentInfo.setOrderNo(transaction.getOutTradeNo());
+        paymentInfo.setTransactionId(transaction.getTransactionId());
+        paymentInfo.setCallbackTime(new Date());
+        paymentInfo.setCallbackContent(JSON.toJSONString(transaction));
+        paymentInfoMapper.updateById(paymentInfo);
+        // 表示交易成功！
+
+        // 后续更新订单状态！ 使用消息队列！
+        rabbitService.sendMessage(MqConst.EXCHANGE_ORDER, MqConst.ROUTING_PAY_SUCCESS, paymentInfo.getOrderNo());
     }
 }
